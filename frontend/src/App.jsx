f2fdc65574ac0175970ae4df70fc8ca7cb8aaa11
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-07-01 14:18:50 +0200

    change root name from app to index.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { GroupProvider } from './context/GroupContext';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import NotFound from './pages/NotFound';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { GroupProvider } from './context/GroupContext'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import NotFound from './pages/NotFound'
 
-import GroupPage from './pages/GroupPage';
-import GroupForm from './components/GroupForm';
-import LoginPage from './pages/LoginPage';
+import GroupPage from './pages/GroupPage'
+import GroupForm from './components/GroupForm'
+import LoginPage from './pages/LoginPage'
 
-import RegisterPage from './pages/RegisterPage';
+import RegisterPage from './pages/RegisterPage'
 
-import VerifyEmailPage from './pages/VerifyEmailPage';
-import NavBar from './components/NavBar';
-import './App.css';
-import { UserProvider } from './context/UserContext';
-import VerifyEmail from './pages/VerifyEmail';
-import { TaskProvider } from './context/TasksContext';
+import VerifyEmailPage from './pages/VerifyEmailPage'
+import NavBar from './components/NavBar'
+import './App.css'
+import { UserProvider } from './context/UserContext'
+import VerifyEmail from './pages/VerifyEmail'
+import { TaskProvider } from './context/TasksContext'
 
-import { MessageProvider } from './context/MessagesContext';
-import RecoverPassword from './pages/RecoverPassword';
+import { MessageProvider } from './context/MessagesContext'
+import RecoverPassword from './pages/RecoverPassword'
 // import ProtectedRoutes from './components/ProtectedRoutes';
 
 function App() {
@@ -30,55 +30,32 @@ function App() {
             <TaskProvider>
               <NavBar />
               <Routes>
+                <Route path="/" element={<HomePage />} />
                 <Route
-                  path='/'
-                  element={<HomePage />}
-                />
-                <Route
-                  path='/password-recovery'
+                  path="/password-recovery"
                   element={<RecoverPassword />}
                 />
-                <Route
-                  path='/profile-page'
-                  element={<ProfilePage />}
-                />
+                <Route path="/profile-page" element={<ProfilePage />} />
+
+                <Route path="/groupboard/:groupId" element={<GroupPage />} />
+                <Route path="/create-group" element={<GroupForm />} />
+                <Route path="/login-page" element={<LoginPage />} />
+                <Route path="/register-page" element={<RegisterPage />} />
 
                 <Route
-                  path='/groupboard/:groupId'
-                  element={<GroupPage />}
-                />
-                <Route
-                  path='/create-group'
-                  element={<GroupForm />}
-                />
-                <Route
-                  path='/login-page'
-                  element={<LoginPage />}
-                />
-                <Route
-                  path='/register-page'
-                  element={<RegisterPage />}
-                />
-
-                <Route
-                  path='/verify-email-page'
+                  path="/verify-email-page"
                   element={<VerifyEmailPage />}
                 />
 
-                <Route
-                  path='/verify/:token'
-                  element={<VerifyEmail />}
-                />
-                <Route
-                  path='*'
-                  element={<NotFound />}></Route>
+                <Route path="/verify/:token" element={<VerifyEmail />} />
+                <Route path="*" element={<NotFound />}></Route>
               </Routes>
             </TaskProvider>
           </GroupProvider>
         </MessageProvider>
       </UserProvider>
     </Router>
-  );
+  )
 }
 
 //UserContext
@@ -86,4 +63,4 @@ function App() {
 //TasksContext
 //MessagesContext
 
-export default App;
+export default App
